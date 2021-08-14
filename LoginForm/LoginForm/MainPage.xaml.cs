e9c201770dde19708953a9fcd99d6403700e1c79
cesarlopezt <cesarlopez2808@gmail.com>
2021-08-14 17:53:37 -0400

    Add project files.

---
+++ LoginForm/LoginForm/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LoginForm
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnLoginClicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(username.Text))
+            {
+                await DisplayAlert("Error", "Username shouldn't be empty!", "Ok");
+            } 
+            else if (string.IsNullOrEmpty(password.Text)) 
+            {
+                await DisplayAlert("Error", "Password shouldn't be empty", "Ok");
+            }
+            else
+            {
+                await DisplayAlert("Welcome back", $"Hi {username.Text}!", "Ok");
+            }
+        }
+    }
+}
